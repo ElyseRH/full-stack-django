3ae6a4597e52b41a97a91c6033758aa0f0a78fed
ElyseRH <elyserhoran@gmail.com>
2021-12-01 12:07:28 +0000

    Add html and django for adding items

---
+++ todo/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Item
+
+
+class ItemForm(forms.ModelForm): #dot notation
+    class Meta: #inner class
+        model = Item
+        fields = ['name', 'done']
+
+#to make sure we can use this form, import it in views.py
